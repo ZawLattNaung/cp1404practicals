7d4cda4ab24786222ab50afcce698d491e871dbe
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-06-05 11:06:10 +0800

    randoms

---
+++ prac_03/randoms.py
@@ -0,0 +1,24 @@
+"""
+
+What did you see on line 1?
+What was the smallest number you could have seen, what was the largest?
+Smallest number= 5
+largest number=20
+
+What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?
+smallest number=3
+largest number=9
+couldn't produce 4
+
+What did you see on line 3?
+What was the smallest number you could have seen, what was the largest?
+smallest number=2.5
+largest number=5.5
+
+Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+"""
+
+import random
+print(random.randint(0,100))
