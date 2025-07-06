e8ee9e967e4e43be1612b9d8deabd078b752f1de
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-06 17:15:28 +0800

    creating class for guitar

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+estimated time=15mins
+actual time=
+"""
+
+
+"""
+creating class for guitar
+"""
+class Guitar:
+    def __init__(self,name="",year="",cost=0.0):
+        self.name=name
+        self.year=year
+        self.cost=cost
