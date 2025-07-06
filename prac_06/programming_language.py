a1f0620205e6ea445f734d26bfe5e5df098ad0a1
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-06 17:08:16 +0800

    adding __str__method

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,6 +1,6 @@
 """
 Estimated time=20mins
-Actual time=
+Actual time=15mins
 """
 
 """Creating class"""
@@ -10,5 +10,10 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
-
+    """returns True/False if the programming language is dynamically typed or not."""
+    def is_dynamic(self):
+        return self.typing.lower() == "dynamic"
+    """adding __str__ method"""
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
