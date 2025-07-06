c308fe81b9ae899847ede51beb4862e8708bfe80
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-06 17:33:58 +0800

    testing vintage method

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -10,3 +10,11 @@ expected_age_another = 12
 actual_age_another = guitar2.get_age()
 print(f"{guitar2.name} get_age() - Expected {expected_age_another}. Got {guitar2.get_age()}")
 
+#Test the is_vintage method
+expected_vintage_gibson = True
+actual_vintage_gibson = guitar1.is_vintage()
+print(f"Gibson L-5 CES is_vintage() - Expected {expected_vintage_gibson}. Got {actual_vintage_gibson}")
+
+expected_vintage_another = False
+actual_vintage_another = guitar2.is_vintage()
+print(f"Another Guitar is_vintage() - Expected {expected_vintage_another}. Got {actual_vintage_another}")
