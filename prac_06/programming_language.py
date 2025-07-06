acc72d942f128171e0caaa764599af9236b6dd37
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-06 17:06:27 +0800

    creating class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""
+Estimated time=20mins
+Actual time=
+"""
+
+"""Creating class"""
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
