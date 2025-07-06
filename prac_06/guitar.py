c6aca0a28712d0dd21db7b7c849ed344cfe7050c
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-06 17:20:36 +0800

    adding get_age

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -8,9 +8,14 @@ actual time=
 creating class for guitar
 """
 class Guitar:
-    def __init__(self,name="",year="",cost=0.0):
+    def __init__(self,name="",year=0,cost=0):
         self.name=name
         self.year=year
         self.cost=cost
+
     def __str__(self): #adding __str__
         return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):#adding get_age to calculate guitar age
+        current_year = 2025  # use datetime to get the current year
+        return current_year - self.year
