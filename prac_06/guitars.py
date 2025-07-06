c85c7ad41028e35cdd4ab0344d39d1f11ad0507c
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-06 17:43:43 +0800

    using vintage and printing out mhr guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,4 +17,12 @@ while name != "":
     # Ask again at the end of the loop
     name = input("Name: ")
 
+# using is_vintage and loop through the list
+print("\nThese are my guitars:")
 
+for guitar in guitars:
+    if guitar.is_vintage():
+        vintage_string = " (vintage)"
+    else:
+        vintage_string=""
+    print(f"{guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
