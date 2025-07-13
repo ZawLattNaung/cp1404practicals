7ffb02f11ef7b0950f54a2a6300621fd7e768f48
ZawLattNaung <zawlattnaung@my.jcu.edu.au>
2025-07-13 17:11:52 +0800

    adding functions and main

---
+++ prac_07/myguitars.py
@@ -0,0 +1,35 @@
+import csv
+from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
+def main():
+    guitars = load_guitars(FILENAME)
+    guitars.sort()  # Sort guitars by year using __lt__ method
+    display_guitars(guitars)
+
+    # Add new guitars from user input
+    print("\nAdd a new guitar (leave name empty to finish):")
+    new_guitar = add_guitar()
+    while new_guitar is not None:
+        guitars.append(new_guitar)
+        new_guitar = add_guitar()
+
+    # Save all guitars back to file
+    save_guitars(FILENAME, guitars)
+    print("\nGuitars have been saved to", FILENAME)
+
+def load_guitars(FILENAME):
+    pass
+
+def display_guitars(guitars):
+    pass
+
+def add_guitar():
+    pass
+
+def save_guitars(filename, guitars):
+    pass
+
+
+if __name__ == "__main__":
+    main()
